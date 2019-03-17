9e1e9ad54c5d3c86775ccdf2aa26a44ad0095965
Hamza Amer <hamer1563@gmail.com>
2019-03-17 18:46:22 -0400

    Create rainJs.js

---
+++ rainJs.js
@@ -0,0 +1,23 @@
+var nbDrop = 858;
+
+// generate random number between the range given
+function randRange(minNum, maxNum){
+
+	return (Math.floor(Math.random() * (maxNum - minNum +1)) + minNum);
+}
+
+function createRain(){
+	for(i = 1; i < nbDrop; i++){
+
+		var dropLeft = randRange(0, 1600);
+		var dropTop = randRange(-1000, 1400);
+
+		$(".rain").append('<div class ="drop" id="drop' + i +'"></div>' );
+		$('#drop'+i).css('left', dropLeft);
+		$('#drop'+i).css('top', dropTop);
+	}
+}
+
+window.onload = function(){
+createRain();
+}
